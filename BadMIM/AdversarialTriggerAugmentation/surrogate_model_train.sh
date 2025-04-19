6193c048053273bbf4cbd1dbe3960598beb6f581
L-de-Yang <230249729@seu.edu.cn>
2025-04-19 15:17:20 +0800

    Update surrogate_model_train.sh

--- BadMIM/AdversarialTriggerAugmentation/surrogate_model_train.sh
+++ BadMIM/AdversarialTriggerAugmentation/surrogate_model_train.sh
@@ -1,2 +1,10 @@
 python 
-surrogate_model_trainer.py
+surrogate_model_trainer.py 
+--surrogate_dataset '' 
+--nb_class 257 
+--batch_size 128 
+--total_epochs 100 
+--seed 0 
+--num_workers 4 
+--output_dir '' 
+--finetune '' 
