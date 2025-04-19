2525b7fe6dad4067bcf3b777a6d2966065fbf63e
L-de-Yang <230249729@seu.edu.cn>
2025-04-19 15:43:46 +0800

    Create run_reconstruction_hijacking.sh

---
+++ BadMIM/ReconstructionHijacking/run_reconstruction_hijacking.sh
@@ -0,0 +1,13 @@
+python 
+badmim.py 
+--seed 0 
+--batch_size 128 
+--device 'cuda' 
+--data_path ''
+--trigger_path '' 
+--output_dir '' 
+--finetune '' 
+--pratio 0.5 
+--alpha 0.2
+--total_epochs 400 
+--warmup_epochs 40 
